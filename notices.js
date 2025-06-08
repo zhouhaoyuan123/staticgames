20edaa846d5418383f452ad66216a9f6b259b2d8
zhouhaoyuan123 <s3462599202@qq.com>
2025-06-08 21:28:53 +0800

    add feedback channel

--- notices.js
+++ notices.js
@@ -13,11 +13,11 @@ window.noticesConfig = {
             id: 1,
             type: "info",
             content: {
-                en: "Welcome to the platform! Enjoy our games.",
+                en: "Welcome to the platform! Enjoy our games. <a href='https://staticgames.rf.gd/poll/index.php/846812?lang=en'>Feedback channel</a> ",
                 es: "¡Bienvenido a la plataforma! Disfruta de nuestros juegos.",
                 fr: "Bienvenue sur la plateforme ! Profitez de nos jeux.",
                 de: "Willkommen auf der Plattform! Viel Spaß mit unseren Spielen.",
-                zh: "欢迎来到平台！尽情享受我们的游戏。"
+                zh: "欢迎来到平台！尽情享受我们的游戏。 <a href='https://staticgames.rf.gd/poll/index.php/356943?lang=zh-Hans'>反馈通道</a>"
             }
         },
         // Add more notices as needed
