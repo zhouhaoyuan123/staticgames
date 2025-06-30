b5fedb767f6444bb43bf0a4573637b9d66300862
zhouhaoyuan123 <s3462599202@qq.com>
2025-06-30 20:38:23 +0800

    add backlink to repo

--- notices.js
+++ notices.js
@@ -13,11 +13,11 @@ window.noticesConfig = {
             id: 1,
             type: "info",
             content: {
-                en: "Welcome to the platform! Enjoy our games. <a href='https://staticgames.rf.gd/poll/index.php/846812?lang=en'>Feedback channel</a> Note: The page might refresh when you change themes, so save your data first! ",
-                es: "¡Bienvenido a la plataforma! Disfruta de nuestros juegos.",
-                fr: "Bienvenue sur la plateforme ! Profitez de nos jeux.",
-                de: "Willkommen auf der Plattform! Viel Spaß mit unseren Spielen.",
-                zh: "欢迎来到平台！尽情享受我们的游戏。 <a href='https://staticgames.rf.gd/poll/index.php/356943?lang=zh-Hans'>反馈通道</a>切换主题前请先保存自己的进度！"
+                en: "Welcome to the platform! Enjoy our games. See the <a href='https://codeberg.org/zhou2012/StaticGames'>repo</a>. <a href='https://staticgames.rf.gd/poll/index.php/846812?lang=en'>Feedback channel</a> Note: The page might refresh when you change themes, so save your data first! ",
+                es: "¡Bienvenido a la plataforma! Disfruta de nuestros juegos. Consulta el <a href='https://codeberg.org/zhou2012/StaticGames'>repositorio</a>. Note : La página puede refrescarse al cambiar de tema, ¡así que guarda tus datos primero!",
+                fr: "Bienvenue sur la plateforme ! Profitez de nos jeux. Voir le <a href='https://codeberg.org/zhou2012/StaticGames'>dépôt</a>. Note : La page peut se rafraîchir lorsque vous changez de thème, donc sauvegardez vos données d'abord !",
+                de: "Willkommen auf der Plattform! Viel Spaß mit unseren Spielen. Siehe das <a href='https://codeberg.org/zhou2012/StaticGames'>Repository</a>. <a href='https://staticgames.rf.gd/poll/index.php/846812?lang=de'>Feedback-Kanal</a> Hinweis: Die Seite könnte sich aktualisieren, wenn Sie das Thema wechseln, also speichern Sie zuerst Ihre Daten!",
+                zh: "欢迎来到平台！尽情享受我们的游戏。请访问我们的<a href='https://codeberg.org/zhou2012/StaticGames'>仓库</a>。 <a href='https://staticgames.rf.gd/poll/index.php/356943?lang=zh-Hans'>反馈通道</a>切换主题前请先保存自己的进度！"
             }
         },
         {
